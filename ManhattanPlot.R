c93993fc242ec7d1fa0e7ca06c5049505c61f35c
LukeAndersonTrocme <luke.anderson.trocme@gmail.com>
2018-08-24 17:15:42 -0400

    added doubletons

--- ManhattanPlot.R
+++ ManhattanPlot.R
@@ -7,6 +7,7 @@ library(multtest)
 
 dir='/Volumes/gravel/luke_projects/1000Genomes/Regression/'
 out='/Volumes/gravel/luke_projects/1000Genomes/MeanDev/'
+
 if(F){ #this is the loop that took the mean of the deviances
 for(chrom in seq(1,22)){
 	print(chrom)
@@ -29,16 +30,30 @@ for(chrom in seq(1,22)){
 	}
 }
 }
+
 #read in rsIDs (used for GWAS catalogue search)
 rsID<-fread('/Users/luke/genomes/genomes/1kGP_4bedFiltered/1kGP_GenomeWide_Chr.Pos.rsID.txt')
 colnames(rsID)<-c('Chr','Pos','rsID')
+
 #read in list of global singletons
-singles<-fread('~/Documents/QualityPaper/Misc/Singletons_Chr.Pos.rsID.txt', col.names=c('Chr','Pos','rsID'))
-singles$single<-'Yes' #dummy variable for merging
+singles<-fread(
+	'~/Documents/QualityPaper/Misc/Singletons_Chr.Pos.rsID.txt',
+	col.names=c('Chr','Pos','rsID'))
+singles$drop<-'Yes' #dummy variable for merging
+	
+doubles<-fread(
+	'/Users/luke/Documents/QualityPaper/Misc/Doubletons.txt',
+	col.names=c('Chr','Pos','rsID'))
+doubles$drop<-'Yes' #dummy variable for merging
+
+OnesTwos<-rbind(singles, doubles)
+rm(singles)
+rm(doubles)
 
 #read in mean deviances for all pops
 Reg <- fread('/Volumes/gravel/luke_projects/1000Genomes/MeanDev/AllPops_Dev.csv', fill=T)
 colnames(Reg)<-c('V1','Chr','Pos','SumDev','MeanDev','Count')
+
 #had duplicate headers, so had to remove them and reset columns to numeric
 Reg$V1<-NULL
 Reg<-Reg[which(Reg$Chr != 'Pos'),]
@@ -48,7 +63,7 @@ Reg$SumDev <-as.numeric(as.character(Reg$SumDev))
 Reg$MeanDev <-as.numeric(as.character(Reg$MeanDev))
 
 #join the singles to the Reg to get info of what sites to remove
-Reg<-left_join(Reg, singles, by=c('Chr','Pos'))
+Reg<-left_join(Reg, OnesTwos, by=c('Chr','Pos'))
 
 rm(singles)
 #get p values from sum of deviances
@@ -58,38 +73,26 @@ Reg$p <- pchisq(Reg$SumDev, Reg$Count, lower.tail=F)
 #write the table to make it easier to load later on
 write.table(Reg, file='~/Documents/QualityPaper/Misc/Reg.txt', quote=F, row.names=F)
 #Reg <- fread('~/Documents/QualityPaper/Misc/Reg.txt')
+
 #this removes the singletons
-NoSingles<-Reg[which(is.na(Reg$single)),]
+NoSingles<-Reg[which(is.na(Reg$drop)),]
 NoSingles<-left_join(NoSingles, rsID, by=c('Chr','Pos')) #get rsID for these sites
+
 ##RICK Adjustment
 procedures <- c( "TSBH") # two-stage Benjamini & Hochberg (2006) step-up FDR-controlling
+#0.05
 adjusted <- mt.rawp2adjp(NoSingles$p, procedures, alpha = 0.05)
-adj<-as.data.frame(adjusted$adj[order(adjusted$index), ])
-NoSingles$adjusted<--log10(adj$TSBH_0.05)
-NoSingles$adjustedP<-adj$TSBH_0.05
+adj <- as.data.frame(adjusted$adj[order(adjusted$index), ])
+NoSingles$adjusted.05 <- -log10(adj$TSBH_0.05)
+NoSingles$adjustedP.05 <- adj$TSBH_0.05
+#0.01
+adjusted <- mt.rawp2adjp(NoSingles$p, procedures, alpha = 0.01)
+adj <- as.data.frame(adjusted$adj[order(adjusted$index), ])
+NoSingles$adjusted.01 <- -log10(adj$TSBH_0.01)
+NoSingles$adjustedP.01 <- adj$TSBH_0.01
 
-adj.less<-NoSingles[which((NoSingles$adjusted < 20)&(NoSingles$Count > 1)),]
+adj.sig.05<-NoSingles[which((NoSingles$adjusted.05 >= -log10(0.05))&(NoSingles$Count > 1)),]
+adj.sig.01<-NoSingles[which((NoSingles$adjusted.01 >= -log10(0.01))&(NoSingles$Count > 1)),]
 
-adj.20<-NoSingles[which((NoSingles$adjusted >= 20)&(NoSingles$Count > 1)),]
-adj.20$adjusted=20
-adj.sig<-NoSingles[which((NoSingles$adjusted >= -log10(0.05))&(NoSingles$Count > 1)),]
-
-write.table(adj.sig, '~/Documents/QualityPaper/Significant6SNPs_adjusted.txt', quote=F, row.names=F)
-
-ggplot(adj.less, aes(x=Pos, y = adjusted, color=as.factor(Chr)))+
-geom_point(data=adj.20, aes(x=Pos, y = plog10,),shape=1)+
-facet_grid(~Chr, scales='free_x', space='free_x', switch='x')+
-scale_fill_manual (values=getPalette(colourCount))+
-scale_y_continuous(expand=c(0,0))+
-geom_point(alpha=0.3, size=1)+theme_classic()+
-geom_hline(yintercept=-log10(0.05), color='blue')+
-labs(y='adjusted -log10(p)', x='Chromosome')+
-theme(plot.title = element_text(hjust = 0.5), 
-plot.subtitle = element_text(hjust = 0.5), 
-axis.text.x=element_blank(), 
-axis.ticks.x=element_blank(),
-axis.line.x=element_blank(), 
-strip.background = element_blank(),
-strip.text.x = element_text(size = 6))+
-guides(color=F)+expand_limits(y=c(0,21))
-ggsave('~/Documents/QualityPaper/ManhattanPlot_adjusted.jpg', height=5, width=10)
+write.table(adj.sig.05, '~/Documents/QualityPaper/Significant0.5SNPs_adjusted.txt', quote=F, row.names=F)
+write.table(adj.sig.01, '~/Documents/QualityPaper/Significant0.1SNPs_adjusted.txt', quote=F, row.names=F)
