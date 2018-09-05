8f2c1b2b568fda2e36c427d65b122ef5f25a7403
LukeAndersonTrocme <luke.anderson.trocme@gmail.com>
2018-09-05 14:24:54 -0400

    updated HapMap + gnomAD SFS

--- Misc/HapMap+GnomAD.R
+++ Misc/HapMap+GnomAD.R
@@ -2,21 +2,27 @@ library(data.table)
 library(ggplot2)
 library(stringr)
 
-GP<-fread('/Users/luke/Documents/QualityPaper/sig/GenomeWide_sig0.01_inHapMap.frq', fill=T)
-HM<-fread('/Users/luke/genomes/genomes/HapMap/hapmap_3.3.b37_Significant0.1SNPs.frq', sep='\t')
+GP<-fread('/Users/luke/Documents/QualityPaper/sig/GenomeWide_sig0.01.frq', skip=1,fill=T,col.names=c('CHROM','POS','count','AC','AF', 'AN'))
+GP$AC1<-round(GP$AC*(1-GP$AF), digits=0)
 
-HM$AF<-str_split_fixed(HM$V4, ";", 3)[,2]
-HM$AF<-as.numeric(as.character(str_split_fixed(HM$AF, "=", 2)[,2]))
+HM<-fread('/Users/luke/genomes/genomes/HapMap/hapmap_3.3.b37_Significant0.1SNPs_format.frq', sep='\t',fill=T, col.names=c('CHROM','POS','rsID','AC','AF', 'AN'))
+HM<-HM[which(complete.cases(HM)==T),]
 
-plot<-merge(GP, HM, by.x=c('CHROM','POS'), by.y=c('V1','V2'))
 
-ggplot(plot, aes(x=V6, y=AF))+geom_point()+geom_point(data=plot[which(plot$V3=='rs6057648'),], color='blue')+labs(x='Allele Frequency in HapMap', y='Allele Frequency in 1kGP', title='Significant SNPs found in HapMap and 1kGP')+theme_classic()
+plot<-merge(GP, HM, by=c('CHROM','POS'))
 
-GN<-fread('/Users/luke/genomes/GnomAD/gnomad_AF.frq', colClasses=c('numeric','numeric','character','numeric','numeric','numeric','numeric','numeric','numeric','numeric','numeric'))
+ggplot(plot, aes(x=1-AF.x, y=AF.y))+geom_point()+geom_point(data=plot[which(plot$rsID=='rs6057648'),], color='blue')+labs(x='Allele Frequency in HapMap', y='Allele Frequency in 1kGP', title='Significant SNPs found in HapMap and 1kGP')+theme_classic()
 
+GN<-fread('/Users/luke/genomes/GnomAD/gnomad_AF.frq')
+GN$AF_raw<-as.numeric(as.character(GN$AF_raw))
+GN$AC_raw<-as.numeric(as.character(GN$AC_raw))
 ggplot(GN, aes(x=reorder(POS,CHROM),y=AF_raw))+geom_point(shape=1)+theme_classic()+theme(axis.text.x=element_blank())
 
 GNm<-melt(GN[,-c('AF_raw')], id=c('CHROM','POS','ID'))
 GNm$value<-as.numeric(as.character(GNm$value))
 GNm<-GNm[which(GNm$value!=0),]
 ggplot(GNm, aes(x=reorder(POS,CHROM),y=value, color=variable))+geom_point(alpha=0.4)+theme_classic()+theme(axis.text.x=element_blank())+labs(y='Frequency',x='Position')
+
+plt<-merge(GP, GN, by=c('CHROM','POS'))
+
+ggplot(plt, aes(y=AC1, x=AC_raw))+geom_point(shape=1)+labs(x='Allele Frequency in GnomAD', y='Allele Frequency in 1kGP', title='Significant SNPs found in GnomAD and 1kGP')+theme_classic()+xlim(c(0,1000))+ylim(c(0,1000))
