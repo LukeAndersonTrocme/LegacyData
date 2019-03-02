04d5fd57277a3a6ab957a3e38b5c15be671cb30b
LukeAndersonTrocme <luke.anderson.trocme@gmail.com>
2019-03-02 15:51:26 -0500

    Added LFA Analysis

--- rick/ProcessVCF.R
+++ rick/ProcessVCF.R
@@ -10,6 +10,8 @@ library(furrr)
 library(glm2)
 library(cowplot)
 library(drake)
+options(future.globals.maxSize= 10000000000)
+future::plan(multicore)
 
 #For running in terminal
 args = commandArgs(trailingOnly=T)
@@ -72,46 +74,135 @@ load('~/Documents/QualityPaper/rick/bas_dt')
 }
                    
 #Load Genotype Data
-#Load VCF file into a sparse matrix                     
-p <- pipe(paste("gzcat ",GenoPath,"ALL.chr",Chr, VCFname," |
+#Load VCF file into a sparse matrix
+to_datalist <- function(i, data_genotype, data_iaf){
+  
+    data <- tibble(genotype=data_genotype[i,],
+               iaf=data_iaf[i,])
+               
+     return(data)       
+}
+get_data <- function(){
+  
+  p <- pipe(paste("gzcat ",GenoPath,"ALL.chr",Chr, VCFname," |
           sed /^#/d  |
           cut  -f '10-' |
           ./a.out | 
           cut -f '1-2'",sep=''))
-x <- read.table(p,
+  x <- read.table(p,
                 colClasses=c("integer","integer"), 
                 fill=TRUE,
                 row.names=NULL)
 
-# Convert to a sparse matrix of people (rows) x variant (columns)
-GT <- sparseMatrix(i=x[,2], j=x[,1], x=1.0)
-                     
-# Logistic factor analysis
-GT_mtx <- t(as.matrix(GT))
-LF <- lfa(GT_mtx, 4)
-print(dim(LF))
-
-#compute Individual Specific Allele Frequency
-sub_ial <- af(GT_mtx, LF)
-                    
-to_datalist <- function(i, data_genotype, data_iaf){
   
-    data <- tibble(genotype = data_genotype[i,], 
-    			   iaf = data_iaf[i,])      
-    return(data)       
+  data <- sparseMatrix(i=x[,2], j=x[,1], x=1L)
+  
+  
+  return(data)
+  
+}
+                     
+get_af_data <- function(data, d=4){
+  
+  # compute the first d factors including the intercept
+  LF <- lfa(data, d)
+  # compute the individual-specific allele frequencies
+  data_iaf <- af(data, LF)
+  loci <- c(1:dim(data)[1])
+  names(loci) <- loci
+  
+  data_list <- future_map(loci, 
+                 to_datalist,
+                 data_genotype=data,
+                 data_iaf=data_iaf) 
+  
+  data_list <- 
+  enframe(data_list,
+          name = "locus", 
+          value = "dataset")
+  
+  return(data_list)
+  
 }
 
-loci <- seq(1,nrow(GT_mtx))
-names(loci) <- loci
+get_predicted_mean <- function(model_fit) {
+ predicted <- 
+   augment(model_fit, type.predict = "response") %>%
+   select("genotype","offset.iaf.",".fitted") %>%
+   rename(iaf="offset.iaf.", mu=".fitted" )
+  return(predicted)
+}
 
-data_list <- map(loci, 
-                 to_datalist,
-                 data_genotype = GT_mtx,
-                 data_iaf = sub_ial)
+get_stats <- function(model_fit){
+  stats_dt <- 
+    tidy(model_fit)  %>%
+    filter(term=="avg_qual_mean") %>%
+    select(estimate, p.value) %>%
+    mutate(deviance= anova(model_fit)["Deviance"][[1]][2])
+  return(stats_dt)
+}
 
-save(data_list, file = paste('~/genomes/genomes/hg19/DataListsDataLists/Chr_',Chr,'_DataList', sep=''))
+fit_model <- function(dataset){
+  
+   fit <- glm(genotype ~ avg_qual + offset(iaf),
+                           family ="binomial",
+                           data=dataset)
+   
+   stats <- get_stats(fit)
+   predicted <- get_predicted_mean(fit)
+   
+   return(list(stats, predicted))
+}
 
-#Get rsID
-p <- pipe(paste("gzcat ",GenoPath,"ALL.chr",Chr, VCFname," | sed /^#/d  | cut  -f '3'",sep=''))         
-rsID <- fread(p, colClasses=c("integer"), fill=TRUE, row.names=NULL)
-save(rsID, file = paste('~/genomes/genomes/hg19/DataListsDataLists/Chr_',Chr,'_rsID', sep=''))
+get_plot <- function(data){
+  
+ p <- ggplot(bind_cols(data, 
+                   bas_dt %>% 
+                     select(Population,
+                            avg_qual_mean))) +
+    geom_point(aes(avg_qual_mean, 
+                   genotype),
+               size=.5,
+               alpha= 0.4) +
+    geom_line(aes(avg_qual_mean,
+                  iaf, 
+                  colour="af"),
+              linetype="dashed") +
+    geom_line(aes(avg_qual_mean,
+                  mu,
+                  colour="mu")) +
+      theme_bw() +
+    facet_wrap(~Population)
+  
+  return(p)
+}
+
+run_workflow <- function(loci){
+
+  data <- get_data()
+  data <- t(as.matrix(data[,loci]))
+  data_list <- get_af_data(data) 
+  
+  rm(data)
+  gc()
+  
+  data_list <- 
+    data_list %>%
+  #slice(1:10) %>%
+  transmute(
+    locus=locus,
+    fit1 = future_map(dataset, fit_model))  %>%
+  transmute(
+    locus=locus,
+    stats = future_map(fit1, c(1)),
+    predicted =future_map(fit1, c(2))) %>%
+    unnest(stats) %>%
+    arrange(-deviance) 
+  
+  return(data_list)
+  
+}
+
+data_list <- run_workflow (loci=c(1:50000))
+
+save(data_list, file = paste('~/genomes/genomes/hg19/DataListsDataLists/Chr_',Chr,'_DataList.Rdata', sep=''))
