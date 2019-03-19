697969b777f2f3d4e7f5df37098b902323c866f0
LukeAndersonTrocme <luke.anderson.trocme@gmail.com>
2019-03-19 12:50:57 -0400

    functional VCF filtration + Split to BED

--- Misc/Parallel_VCFtoBED.py
+++ Misc/Parallel_VCFtoBED.py
@@ -1,26 +1,53 @@
 import multiprocessing as mp
 import sys, os
-import time
-
 
 def Bash_cmd(i):
-    #print statement to keep track of where you are
-    print('Working on Chrom : '+str(i))
-    #put your bash command below
+    #Set Path and vcfName
     path='/Users/luke/genomes/genomes/hg19/'
-    os.system('/Users/luke/bin/plink_mac/plink \
-    --vcf {0}/phase3//ALL.chr{1}.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.vcf.gz \
-    --make-bed \
-    --out {0}/plink/chr{1}'.format(path, i))
+    vcfName='.phase3_shapeit2_mvncall_integrated_v5a.20130502.genotypes.'
+
+    #Filter Allele Frequency
+    fname = '{0}/AF_VCF/chr{1}_AF.vcf.gz'.format(path, i)
+    if os.path.isfile(fname) :
+        print('VCF already Filtered : '+str(i))
+    else :
+        print('AF Filter Chrom : '+str(i))
+        os.system('/usr/local/bin/bcftools-1.6/bcftools filter \
+        {0}/phase3/ALL.chr{1}{2}vcf.gz \
+        -e "MAF > 0.9988 || MAF < 0.001198" \
+        -Oz -o {0}/AF_VCF/chr{1}_AF.vcf.gz'.format(path, i,vcfName))
+
+    fname = '{0}/AF_VCF/Split_chr{1}_aa.vcf'.format(path, i)
+    if os.path.isfile(fname) :
+        print('VCF already Split : '+str(i))
+    else :
+        print('Split Chrom : '+str(i))
+        os.system('gzcat {0}/AF_VCF/chr{1}_AF.vcf.gz \
+        | head -1000 | grep "^#" \
+        > {0}/AF_VCF/chr{1}_header ; \
+        zgrep -v "^#" {0}/AF_VCF/chr{1}_AF.vcf.gz \
+        > {0}/AF_VCF/chr{1}_variants ; \
+        split -l 100000 \
+        {0}/AF_VCF/chr{1}_variants \
+        {0}/AF_VCF/Split_chr{1}_'.format(path, i))
+
+        print('Combine Chrom : '+str(i))
+        os.system('for i in {0}/AF_VCF/Split_chr{1}_*; \
+        cat {0}/AF_VCF/chr{1}_header $i \
+        > $i.vcf && rm -f $i ; done ; \
+        rm -f {0}/AF_VCF/chr{1}_header {0}/AF_VCF/chr{1}_variants'.format(path, i))
+
+    print('Plink Chrom : '+str(i))
+
+    os.system('for j in {0}/AF_VCF/Split_chr{1}_*.vcf; \
+    do /Users/luke/bin/plink_mac/plink \
+    --vcf $j --make-bed --out ${{j%.vcf}} ; \
+    done ; rm -f $j'.format(path, i))
 
 if __name__ == '__main__':
-    #you can replace ListOfChrom with whatever you are iterating through
     ListOfChrom=list(range(1,23))
-    #pool_size is number of CPU
     pool_size=7 #mp.cpu_count()
     pool=mp.Pool(processes=pool_size)
-    #this is where you are calling the parallelization
     pool_outputs= pool.map(Bash_cmd, ListOfChrom)
-    #some magic, not sure what this does
     pool.close()
     pool.join()
